a59de3c05fda51c8ef3fbc8c8004742a9914db87
LiranBratt2121 <brattlirannin@gmail.com>
2023-08-22 13:59:35 +0300

    main: Add bumper detection model

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 import cv2
 from ultralytics import YOLO
 
-def click_event(event, x, y, flags, params):
-    if event == cv2.EVENT_LBUTTONDOWN:
-        print(x, ' ', y)
-        
-model = YOLO(r'weights\best.pt')
+bumper_model = YOLO(r'bumper_weights\best.pt')
+tracker = DeepSort(max_iou_distance=10, max_age=100)
 cap = cv2.VideoCapture(r'test_videos\Final 1 dis 1 ISR - Made with Clipchamp.mp4')
 object_detector = cv2.createBackgroundSubtractorMOG2(100, 100, False)
 
@@ -13,35 +10,16 @@ detections = []
 
 while True:
     ret, frame = cap.read()
-    roi = frame[240:504, 151:1217] ## DIS 1 ROI
-    # roi = frame[191:406, 161:1200] ## DIS 4 ROI 
-    
-    mask = object_detector.apply(roi)
-
-    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
-    mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
-    mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
-
-    cv2.imshow('Mask', mask)
-
-    detections.clear()
-
-    contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-    for cnt in contours:
-        area = cv2.contourArea(cnt)
-
-        if area > 700:
-            x, y, w, h = cv2.boundingRect(cnt)
-            detections.append([x, y, w, h])
-
-    detections.sort(key=lambda det: det[2] * det[3], reverse=True)
-
-    for det in detections[:6]:
-        x, y, w, h = det
-        cv2.rectangle(roi, (x, y), (x + w, y + h), (0, 255, 0), 3)
-
-        det_frame = roi[y:y + h, x:x + w]
+    for result in results.boxes.data.tolist():
+        x1, y1, x2, y2, score, class_id = result
+        
+        if score > threshold:
+            cv2.rectangle(roi, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 2)
+                        
+        det_frame = roi[int(y1):int(y2), int(x1):int(x2)]
+        
+        detections.append([[x1, y1, x2 - x1, y2 - y1], score, class_id])
+        
         
         results = model(det_frame)[0]
 
