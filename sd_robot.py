2bd85a6e9d688738398f3e48f908f0057fbe5d06
sagi <sagi21805@gmail.com>
2023-10-21 15:48:07 +0300

    added robot class

--- sd_robot.py
+++ sd_robot.py
@@ -8,5 +8,10 @@ class Robot():
         self.cones = np.zeros(3, dtype=np.uint8) #[High, Mid, Low]
         self.cubes = np.zeros(3, dtype=np.uint8) #[High, Mid, Low]
         self.trajectory = []
+        
+    def set_position(self, pt1: np.ndarray, pt2: np.ndarray):
+        self.pt1 = pt1
+        self.pt2 = pt2
     
-    
+    def get_position(self):
+        return (self.pt1, self.pt2)
