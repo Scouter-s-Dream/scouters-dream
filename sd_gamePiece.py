cd388f2328dfd60c348a77b0dcd7444c3597bb98
sagi <sagi21805@gmail.com>
2023-10-22 11:04:45 +0300

    added some util funcs

--- sd_gamePiece.py
+++ sd_gamePiece.py
@@ -1,20 +1,75 @@
 from ultralytics import YOLO
 from sd_robot import Robot
 import numpy as np
-
-GREED_POSITION = (np.array([0, 0]), np.array(0, 0))
+from sd_utils import center, distance
+from sd_globals import *
 
 class GamePiece():
     
-    def __init__(self,id:int, type:bool, position: tuple[np.ndarray, np.ndarray]):
+    
+    def __init__(self,id:int, type:bool, position: tuple[np.ndarray[int, int], np.ndarray[int, int]]):
         self.id = id
         self.type = type
         self.position = position
+        self.center = center(position)
+        self.set_side()
         
-    def update_position(self, position: tuple[np.ndarray, np.ndarray]):
+        
+    def update_position(self, position: tuple[np.ndarray[int, int], np.ndarray[int, int]]):
         self.position = position
         
-    def get_position(self):
+    def get_position(self) -> tuple[np.ndarray[int, int], np.ndarray[int, int]]:
         return self.position   
  
+    def is_red(self) -> bool:
+        if self.position[0][0] < RED_COMMUNITY_POSITION[1][0] and self.position[1][0] > RED_COMMUNITY_POSITION[0][0] and self.position[0][1] < RED_COMMUNITY_POSITION[1][1] and self.position[1][1] > RED_COMMUNITY_POSITION[0][1]:
+            return True
+
+    def is_blue(self) -> bool:
+        if self.position[0][0] < BLUE_COMMUNITY_POSITION[1][0] and self.position[1][0] > BLUE_COMMUNITY_POSITION[0][0] and self.position[0][1] < BLUE_COMMUNITY_POSITION[1][1] and self.position[1][1] > BLUE_COMMUNITY_POSITION[0][1]:
+            return True
+
+    def set_side(self):
+        
+        if self.is_blue():
+            GAMEPIECE_BLUESIDE.append(self)
+            self.blue = True
+            self.red = False
+            return
+        
+        if self.is_red():
+            GAMEPIECE_REDSIDE.append(self)
+            self.red = True
+            self.blue = False
+            return
+        
+        self.blue = False
+        self.red = False
+        
+    def closet_redRobot(self) -> Robot:
+        closet_robot = None
+        min_dist = np.inf
+        for robot in ROBOT_REDSIDE:
+            dist = distance(self.center, robot.center) 
+            if dist < min_dist:
+                closet_robot = robot
+                min_dist = dist
+                
+        return closet_robot
+                
+    def closet_blueRobot(self) -> Robot:
+        closet_robot = None
+        min_dist = np.inf
+        for robot in ROBOT_BLUESIDE:
+            dist = distance(self.center, robot.center) 
+            if dist < min_dist:
+                closet_robot = robot
+                min_dist = dist
+                
+        return closet_robot            
+
+    # def closet_blueRobot(self)
+    
+    
+        
     
--- sd_robot.py
+++ sd_robot.py
@@ -1,17 +1,24 @@
 import numpy as np
+from sd_utils import center
+from sd_globals import *
 
 class Robot():
     
-    def __init__(self, number:int, team: bool, position: tuple[np.ndarray, np.ndarray]):
+        
+    def __init__(self, number:int, team: bool, position: tuple[np.ndarray[int, int], np.ndarray[int, int]]):
         self.number = number
-        self.team = team #red = False, blue = True
+        if team:
+            ROBOT_BLUESIDE.append(self)
+        else:
+            ROBOT_REDSIDE.append(self)
         self.position = position
         self.cones = np.zeros(3, dtype=np.uint8) #[High, Mid, Low]
         self.cubes = np.zeros(3, dtype=np.uint8) #[High, Mid, Low]
         self.trajectory = []
+        self.center = center(position)
         
     def update_position(self, position: tuple[np.ndarray, np.ndarray]):
         self.position = position
         
-    def get_position(self):
+    def get_position(self) -> np.ndarray[int, int]:
         return self.position
