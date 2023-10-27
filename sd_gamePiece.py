cc26b59856143f918abe25e3ae1fc50f2589703a
sagi <sagi21805@gmail.com>
2023-10-27 10:56:29 +0300

    sd_gamePiece: ADD documentation

--- sd_gamePiece.py
+++ sd_gamePiece.py
@@ -8,29 +8,45 @@ class GamePiece():
     
     
     def __init__(self,id:int, type:bool, position: tuple[np.ndarray[int, int], np.ndarray[int, int]]):
+        """constractor for the GamePiece class
+
+        Args:
+            id (int): number to identify the game piece in tracking
+            type (bool): cone = False, cube = True
+            position (tuple[np.ndarray[int, int], np.ndarray[int, int]]): the position of the bounding box of the game piece
+        """
+        
         self.id = id
         self.type = type
         self.position = position
         self.center = center(position)
-        self.set_side()
-        
-        
-    def update_position(self, position: tuple[np.ndarray[int, int], np.ndarray[int, int]]):
-        self.position = position
-        
-    def get_position(self) -> tuple[np.ndarray[int, int], np.ndarray[int, int]]:
-        return self.position   
+        self.set_side() 
  
     def is_red(self) -> bool:
+        """checks if the game piece is in the red community
+
+        Returns:
+            bool: True if Red, False else
+        """
         if self.position[0][0] < RED_COMMUNITY_POSITION[1][0] and self.position[1][0] > RED_COMMUNITY_POSITION[0][0] and self.position[0][1] < RED_COMMUNITY_POSITION[1][1] and self.position[1][1] > RED_COMMUNITY_POSITION[0][1]:
             return True
+        else:
+            return False
 
     def is_blue(self) -> bool:
+        """checks if the game piece is in the Blue community
+
+        Returns:
+            bool: True if Blue, False else
+        """
         if self.position[0][0] < BLUE_COMMUNITY_POSITION[1][0] and self.position[1][0] > BLUE_COMMUNITY_POSITION[0][0] and self.position[0][1] < BLUE_COMMUNITY_POSITION[1][1] and self.position[1][1] > BLUE_COMMUNITY_POSITION[0][1]:
             return True
+        else:
+            return False
 
     def set_side(self):
-        
+        """checks if a game piece is in the Blue Community or in the Red Community
+        """
         if self.is_blue():
             GAMEPIECE_BLUESIDE.append(self)
             self.blue = True
@@ -47,6 +63,11 @@ class GamePiece():
         self.red = False
         
     def closet_redRobot(self) -> Robot:
+        """checks the closest Red Robot to the GamePiece
+
+        Returns:
+            Robot: the Robot instance Closet to the GamePiece
+        """
         closet_robot = None
         min_dist = np.inf
         for robot in ROBOT_REDSIDE:
@@ -58,6 +79,11 @@ class GamePiece():
         return closet_robot
                 
     def closet_blueRobot(self) -> Robot:
+        """checks the closest Blue Robot to the GamePiece
+
+        Returns:
+            Robot: the Robot instance Closet to the GamePiece
+        """
         closet_robot = None
         min_dist = np.inf
         for robot in ROBOT_BLUESIDE:
