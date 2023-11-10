7dbe596d291e9fb5156160fd2112a917d9142158
sagi <sagi21805@gmail.com>
2023-11-10 23:44:23 +0200

    MOD: modifed rapper to work with the current code

--- utils/c++/src/cppWraper.cpp
+++ utils/c++/src/cppWraper.cpp
@@ -1,22 +1,8 @@
-#include "field.hpp"
-#include "robot.hpp"
 #include "imgAlgsCpu.hpp"
 #include "tracker.hpp"
 
 extern "C"{
 
-    Field* _Field(const char *path){
-        return new Field(path);
-    }
-    void run(Field* f){
-        f -> run();
-    }
-    Robot* _RobotDefualt(){
-        return new Robot();
-    }
-    Robot* _Robot(int teamNumber, bool team, int p1[], int p2[]){
-        return new Robot(teamNumber,team ,p1 ,p2);
-    }
     Tracker* _Tracker(){
         return new Tracker();
     }
