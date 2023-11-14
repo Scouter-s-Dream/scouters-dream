fa4beb24b7d7a59ee1b81924eec53c8bd50a76bb
sagi <sagi21805@gmail.com>
2023-11-15 00:56:06 +0200

    NEW: added support in new funcs, NEED TEST

--- Tracker/c++/src/cppWraper.cpp
+++ Tracker/c++/src/cppWraper.cpp
@@ -3,8 +3,8 @@
 
 extern "C"{
 
-    Tracker* _Tracker(){
-        return new Tracker();
+    Tracker* _Tracker(int* pointsWithClass, int size, int* img, int width, int height){
+        return new Tracker(pointsWithClass, size, img, width, height);
     }
     void _setTrackPoints(Tracker* t, int* points, int size){
         t -> setTrackPoints(points, size);
