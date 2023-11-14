bad50d74adf082da1bb87b5e13f913145121d488
sagi <sagi21805@gmail.com>
2023-11-15 00:55:16 +0200

    NEW: added utils funcs and new velocity value in the node

--- Tracker/c++/include/linkedList.hpp
+++ Tracker/c++/include/linkedList.hpp
@@ -7,16 +7,18 @@ typedef struct Node Node;
 
 struct Node{
     
+    unsigned short velocity;
     BoundingBox boundingBox;
     struct Node *next;
 
 };
 
-Node* NodeConstractor(BoundingBox boundingBox);
+Node* NodeConstractor(BoundingBox boundingBox, unsigned short velocity);
+
+Node* emptyNode();
 
 class LinkedList{
 
-    //First item
     public: 
         Node* start;
         Node* end;
@@ -24,18 +26,21 @@ class LinkedList{
 
         LinkedList();
 
-        LinkedList(Node *heapNode);
+        LinkedList(Node Node);
+
+        LinkedList(BoundingBox boundingBox, unsigned short velocity);
 
         Node getItem(unsigned int index);
 
-        void append(BoundingBox boundingBox);
+        void append(BoundingBox boundingBox, unsigned short velocity);
 
         void remove(unsigned int index);
 
-        void prepend(BoundingBox boundingBox);
+        void prepend(BoundingBox boundingBox, unsigned short velocity);
 
-        void insert(BoundingBox boundingBox, unsigned int index);
+        void insert(unsigned int index, BoundingBox boundingBox, unsigned short velocity);
 
+        ~LinkedList(); //TODO create destructor that frees all the list.
 
 };
 
--- Tracker/c++/src/linkedList.cpp
+++ Tracker/c++/src/linkedList.cpp
@@ -1,27 +1,41 @@
 #include "linkedList.hpp"
 
-Node* NodeConstractor(BoundingBox boundingBox){
+Node* NodeConstractor(BoundingBox boundingBox, unsigned short velocity){
 
     //TODO Turn into const, sizof is just a useless clac because it's const
     Node* newNode = (Node*) malloc(sizeof(Node));
+    newNode->velocity = velocity;
     newNode->boundingBox = boundingBox;
     newNode->next = nullptr;
     return newNode;
 }
 
-LinkedList::LinkedList(){
-    this->start->next = nullptr;
-    this->end->next = nullptr;
+Node* emptyNode(){
+    Node* newNode = (Node*) malloc(sizeof(Node));
+    newNode->next = nullptr;
+    return newNode;
 }
 
-LinkedList::LinkedList(Node *node){
-    this->start->next = node;
-    this->end->next = node;
+LinkedList::LinkedList(){
+    Node* newNode = emptyNode();
+    this->start = newNode;
+    this->end = newNode;
+}
+
+LinkedList::LinkedList(BoundingBox boundingBox, unsigned short velocity){
+    Node* newNode = NodeConstractor(boundingBox, velocity);
+    this->start = newNode;
+    this->end = newNode;
+}
+
+LinkedList::LinkedList(Node node){ //node needs to be alocated on the heap!
+    this->start = &node;
+    this->end = &node;
 }
 
 Node LinkedList::getItem(unsigned int index){
 
-    if (index < this->length && index > 0){
+    if (index > this->length || index < 0){
         throw "Out of Bounds";
     }
 
@@ -35,16 +49,16 @@ Node LinkedList::getItem(unsigned int index){
 
 }
 
-void LinkedList::append(BoundingBox boundingBox){
-    Node* newNode = NodeConstractor(boundingBox);
+void LinkedList::append(BoundingBox boundingBox, unsigned short velocity){
+    Node* newNode = NodeConstractor(boundingBox, velocity); 
     this->end->next = newNode;    
     this->end = newNode;
-    this->length += 1;
+    this->length++;
 }
 
 void LinkedList::remove(unsigned int index){
     
-    if (index < this->length && index > 0){
+    if (index > this->length || index < 0){
         throw "Out of Bounds";
     }
 
@@ -52,12 +66,8 @@ void LinkedList::remove(unsigned int index){
 
     if (index == 0){
         this->start = ref->next;
-        free(ref); //TODO make delete func 
+        free(ref);
         return;
-    }
-
-    if (index == this->length){
-
     }
 
     for (unsigned int i = 0; ref != nullptr && i < index - 1; i++){
@@ -67,13 +77,38 @@ void LinkedList::remove(unsigned int index){
     Node* next = ref->next->next;
     free(ref->next);
     ref->next = next;
+    this->length--;
     
 }
 
-void LinkedList::prepend(BoundingBox boundingBox){
-
+void LinkedList::prepend(BoundingBox boundingBox, unsigned short velocity){
+    Node* newNode = NodeConstractor(boundingBox, velocity); 
+    newNode->next = this->start;    
+    this->start = newNode;
+    this->length++;
 }
 
-void LinkedList::insert(BoundingBox boundingBox, unsigned int index){
+void LinkedList::insert(unsigned int index, BoundingBox boundingBox, unsigned short velocity){
+    if (index > this->length || index < 0){
+        throw "Out of Bounds";
+    }
 
+    Node* newNode = NodeConstractor(boundingBox, velocity); 
+
+    Node* ref = this->start;
+
+    if (index == 0){
+        this->prepend(boundingBox, velocity);
+    }
+    
+    if (index == this->length){
+        this->append(boundingBox, velocity);
+    }
+
+    for (unsigned int i = 0; ref != nullptr && i < index - 1; i++){
+        ref = ref->next;
+    }
+
+    newNode->next = ref->next;
+    ref->next = newNode;
 }
