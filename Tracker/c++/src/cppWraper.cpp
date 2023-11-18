f0f31879e6323be62715d4cb43ecd3f03024238a
sagi <sagi21805@gmail.com>
2023-11-19 00:00:08 +0200

    del: removed unused functions

--- Tracker/c++/src/cppWraper.cpp
+++ Tracker/c++/src/cppWraper.cpp
@@ -7,14 +7,6 @@ extern "C"{
         return new Tracker(pointsWithClass, size, img, rows, cols, visualize);
     }
 
-    void _setImg(Tracker *t, uint8_t* img){
-        t -> setImg(img);
-    }
-
-    void _setTrackPoints(Tracker *t, uint16_t* pointsWithClasses, uint size){
-        t -> setTrackPoints(pointsWithClasses, size);
-    }
-
     void _track(Tracker *t, uint16_t* pointsWithClasses, uint16_t size, uint8_t* img){
         t -> track(pointsWithClasses, size, img);
     }
