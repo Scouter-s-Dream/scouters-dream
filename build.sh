d9eeb012359727c404747efd6df191a16232da30
sagi <sagi21805@gmail.com>
2023-11-25 17:25:33 +0200

    new: combined the build and tracker scripts to one

--- build.sh
+++ build.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
 cd Tracker
+
 cmake -B ./build
+
 cd build 
+
 make -j4
+
 mv libTracker.so ../libTracker.so
+
+echo "[INFO]: Finished building running python!"
+
+cd ..
+cd ..
+
+python Tracker/python/recognizeBumpersTest.py
--- tracker.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-python3.11 Tracker/python/recognizeBumpersTest.py
