82a2f105fc942cdf2288af14d398f2e9c0890c47
sagi <sagi21805@gmail.com>
2023-11-28 17:30:13 +0200

    maint: adapted to new changes

--- Tracker/python/tracker.py
+++ Tracker/python/tracker.py
@@ -4,12 +4,11 @@ import numpy as np
 class Tracker():        
     
     @staticmethod
-    def YoloToPointsWithClasses(YOLO_result, point_size):
-        boxes = YOLO_result.boxes.cpu().numpy()
-        xywhWithCls = np.concatenate((boxes.xywh, np.reshape(boxes.cls, (boxes.cls.shape[0], 1))), 1)       
-        points = np.array(xywhWithCls[xywhWithCls[:, 0].argsort()], dtype=np.uint16).flatten()
-        
-        return (points, len(points)//point_size)
+    def YoloToPointsAndTypes(YOLO_result):
+        boxes = YOLO_result.boxes.cpu().numpy()      
+        points = np.array(boxes.xyxy.flatten(), dtype=np.uint16)
+        types = np.array(boxes.cls.flatten(), dtype=np.uint16)
+        return (points, types, len(types))
     
     @staticmethod
     def imgToArray(coloredImg: np.ndarray):
@@ -19,10 +18,10 @@ class Tracker():
     @staticmethod
     def setFuncs():
         
-        cpp.lib._Tracker.argtypes = [cpp.uint16_array, cpp.uint16, cpp.uint8_array, cpp.uint16, cpp.uint16, cpp.bool]
+        cpp.lib._Tracker.argtypes = [cpp.uint16_array, cpp.uint16_array, cpp.uint16, cpp.uint8_array, cpp.uint16, cpp.uint16, cpp.bool]
         cpp.lib._Tracker.restype = cpp.object
         
-        cpp.lib._track.argtypes = [cpp.object, cpp.uint16_array, cpp.uint16, cpp.uint8_array]
+        cpp.lib._track.argtypes = [cpp.object, cpp.uint16_array, cpp.uint16_array, cpp.uint16, cpp.uint8_array]
         
         
         
@@ -32,13 +31,13 @@ class Tracker():
         
         Tracker.setFuncs()
         
-        points, size = Tracker.YoloToPointsWithClasses(YOLO_result, 5)
+        points, types, size = Tracker.YoloToPointsAndTypes(YOLO_result)
 
-        self.obj = cpp.lib._Tracker(points, cpp.c_uint16(size), Tracker.imgToArray(coloredImg), cpp.c_uint16(coloredImg.shape[0]), cpp.c_uint16(coloredImg.shape[1]), cpp.c_bool(visualize))
+        self.obj = cpp.lib._Tracker(points, types, cpp.c_uint16(size), Tracker.imgToArray(coloredImg), cpp.c_uint16(coloredImg.shape[0]), cpp.c_uint16(coloredImg.shape[1]), cpp.c_bool(visualize))
     
     def track(self, YOLO_result, coloredImg: np.ndarray):
-        points, size = Tracker.YoloToPointsWithClasses(YOLO_result, 5)
-        cpp.lib._track(self.obj, points, cpp.c_uint16(size), Tracker.imgToArray(coloredImg))
+        points, types, size = Tracker.YoloToPointsAndTypes(YOLO_result)
+        cpp.lib._track(self.obj, points, types, cpp.c_uint16(size), Tracker.imgToArray(coloredImg))
     
     
     
