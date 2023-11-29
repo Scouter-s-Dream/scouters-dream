b60d61dacabcfc5f53f9f0350b71ff7e86289249
sagi <sagi21805@gmail.com>
2023-11-29 14:30:13 +0200

    handled stash issues

--- Tracker/c++/src/cppWraper.cpp
+++ Tracker/c++/src/cppWraper.cpp
@@ -1,14 +1,13 @@
-#include "imgAlgsCpu.hpp"
 #include "tracker.hpp"
 
 extern "C"{
 
-    Tracker* _Tracker(uint16_t* pointsWithClass, uint16_t size, uint8_t* img, uint16_t rows, uint16_t cols, bool visualize){
-        return new Tracker(pointsWithClass, size, img, rows, cols, visualize);
+    Tracker* _Tracker(uint16_t* points, uint16_t* types, uint16_t size, uint8_t* img, uint16_t rows, uint16_t cols, bool visualize){
+        return new Tracker(points, types, size, img, rows, cols, visualize);
     }
 
-    void _track(Tracker *t, uint16_t* pointsWithClasses, uint16_t size, uint8_t* img){
-        t -> track(pointsWithClasses, size, img);
+    void _track(Tracker *t, uint16_t* points, uint16_t* types, uint16_t size, uint8_t* img){
+        t -> track(points, types, size, img);
     }
 
 }
