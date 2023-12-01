fd94d1d3f180b3ece08097eb55eec11d3ccae7f5
sagi <sagi21805@gmail.com>
2023-12-01 20:38:02 +0200

    feat: moved functions to cpp file for readability

--- Tracker/c++/include/vectorFuncs.hpp
+++ Tracker/c++/include/vectorFuncs.hpp
@@ -7,28 +7,8 @@
 using std::cout;
 using std::vector;
 
-template<typename T2>
-void removeAtIndex(vector<T2> &vect, uint16_t index);
+template<typename T> void removeAtIndex(vector<T> &vect, uint16_t index);
 
-template<typename T2>
-void removeAtIndex(vector<T2> &vect, uint16_t index){    
-    //TODO add exception on size.
-    vect.erase(vect.begin() + index);
-}
-
-template <typename T>
-std::ostream& operator<<(std::ostream& os, const std::vector<T>& vector);
-
-template <typename T>
-std::ostream& operator<<(std::ostream& os, const std::vector<T>& vector){
-    // Printing all the elements
-    // using <<
-    os << "[";
-    for (auto element : vector) {
-        os << element << " ";
-    }
-    os << "\b]";
-    return os;
-}
+template <typename T> std::ostream& operator<<(std::ostream& os, const std::vector<T>& vector);
 
 #endif
